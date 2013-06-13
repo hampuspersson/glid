6e490bd8a4d3c5cbfac8316f4ea93fd9a9a9602e
Hampus Persson <hampus@hampuspersson.se>
2013-06-13 22:00:23 +0200

    Basic structure of js-file

---
+++ jquery.glid.js
@@ -0,0 +1,18 @@
+/*global jQuery */
+/*!
+* Glid 1.0
+*
+* Copyright 2013, Hampus Persson - http://hampuspersson.se
+* Released under the WTFPL license - http://sam.zoy.org/wtfpl/
+*
+*/
+
+(function( $ ){
+
+  "use strict";
+
+  $.fn.glid = function( options ) {
+
+  };
+
+})( jQuery );
