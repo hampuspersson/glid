290a7762bc82318aff3c34a4925106cdb719f664
Hampus Persson <hampus@hampuspersson.se>
2013-06-14 10:22:38 +0200

    Refactoring var names for clarity

--- jquery.glid.js
+++ jquery.glid.js
@@ -29,37 +29,39 @@
       $.extend( defaults, options );
     }
 
-    var slider        = this,
-        slides        = slider.children('li'),
+    // Variables relating to the slider of Glid
+    var slider         = {
 
-        ctrl_wrapper  = null,
-        ctrl_next     = null,
-        ctrl_prev     = null;
-
-    // Variables relating to the state of Glid
-    var state         = {
-
-      slidecount      : slides.length - 1, // Index is zero-based, let's make slidecount compatible
+      $el             : this,
+      slidecount      : 0,
       currentindex    : 0,
       nextindex       : 0,
       animating       : false,
 
     };
 
+    var $slides        = slider.$el.children('li'),
+
+        $ctrl_wrapper  = null,
+        $ctrl_next     = null,
+        $ctrl_prev     = null;
+
+    slider.slidecount = $slides.length - 1; // Index is zero-based, let's make slidecount compatible
+
     var init = function() {
 
       // Add classes to all items that are to be a part of the slider
-      slider.wrap('<div class="glid_wrapper">');
-      slides.addClass('glid__item');
+      slider.$el.wrap('<div class="glid_wrapper">');
+      $slides.addClass('glid__item');
       // Show the first item in the slider
-      slides.eq(state.currentindex).fadeIn(settings.animduration);
+      $slides.eq(slider.currentindex).fadeIn(settings.animduration);
 
       // Setup timer to assure that the first image is loaded before the controls are loaded.
       // Height must be set so that the controls can be positionend below
       var img_load_timer = setInterval( function() {
-        if( slides.eq(state.currentindex).children('img').height() ) {
+        if( $slides.eq(slider.currentindex).children('img').height() ) {
           clearInterval( img_load_timer );
-          slider.height( slides.eq(state.nextindex).height() );
+          slider.$el.height( $slides.eq(slider.nextindex).height() );
           init_controls();
         }
       }, 100);
@@ -68,12 +70,12 @@
     var init_controls = function () {
 
       // Setup the controllers
-      ctrl_wrapper  = $('<ul class="glid-controls"></ul>');
-      ctrl_prev     = $('<li class="glid-controls__item"><a href="#" data-direction="prev">Previous</a></li>');
-      ctrl_next     = $('<li class="glid-controls__item"><a href="#" data-direction="next">Next</a></li>');
+      $ctrl_wrapper  = $('<ul class="glid-controls"></ul>');
+      $ctrl_prev     = $('<li class="glid-controls__item"><a href="#" data-direction="prev">Previous</a></li>');
+      $ctrl_next     = $('<li class="glid-controls__item"><a href="#" data-direction="next">Next</a></li>');
 
       // Bind controller click event
-      ctrl_wrapper.on('click', 'a', function(e) {
+      $ctrl_wrapper.on('click', 'a', function(e) {
 
         e.preventDefault();
         var direction = $(this).attr('data-direction');
@@ -83,44 +85,44 @@
       });
 
       // Add controllers to DOM
-      ctrl_prev.appendTo( ctrl_wrapper );
-      ctrl_next.appendTo( ctrl_wrapper );
-      ctrl_wrapper.insertAfter( slider );
+      $ctrl_prev.appendTo( $ctrl_wrapper );
+      $ctrl_next.appendTo( $ctrl_wrapper );
+      $ctrl_wrapper.insertAfter( slider.$el );
 
     };
 
     var run_animation = function( direction ) {
 
       // Don't animate unless we're ready!
-      if( !state.animating ) {
-        state.animating = true;
+      if( !slider.animating ) {
+        slider.animating = true;
 
         // Advance index
         if( direction === 'next' ) {
-          state.nextindex = state.currentindex + 1;
+          slider.nextindex = slider.currentindex + 1;
         } else if( direction === 'prev' ) {
-          state.nextindex = state.currentindex - 1;
+          slider.nextindex = slider.currentindex - 1;
         }
 
         // Make sure that nextindex isn't out of bounds and if it is, reset it.
-        if( state.nextindex < 0 )  {
-          state.nextindex = state.slidecount;
-        } else if( state.nextindex > state.slidecount ) {
-          state.nextindex = 0;
+        if( slider.nextindex < 0 )  {
+          slider.nextindex = slider.slidecount;
+        } else if( slider.nextindex > slider.slidecount ) {
+          slider.nextindex = 0;
         }
 
         // Hide current slide and display next
-        slides.eq(state.currentindex).fadeOut(settings.animduration);
-        slides.eq(state.nextindex).fadeIn(settings.animduration);
+        $slides.eq(slider.currentindex).fadeOut(settings.animduration);
+        $slides.eq(slider.nextindex).fadeIn(settings.animduration);
 
         // Animate the height change of the slider to make sure the controls are moved
-        slider.animate( {
-          height: slides.eq(state.nextindex).height()
+        slider.$el.animate( {
+          height: $slides.eq(slider.nextindex).height()
         });
 
-        // Reset states for next animation
-        state.currentindex  = state.nextindex;
-        state.animating     = false;
+        // Reset sliders for next animation
+        slider.currentindex  = slider.nextindex;
+        slider.animating     = false;
       }
     };
 
